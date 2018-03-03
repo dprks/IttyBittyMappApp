6518814a6894658e10b160b858669837ce5ec900
Dan <dan@domesticcat.com.au>
2018-03-03 15:36:12 +1100

    Scaffolding - Dagger Retrofit module

---
+++ app/src/main/java/au/com/parkinson/dan/ittybittymappapp/data/network/NetModule.java
@@ -0,0 +1,59 @@
+package au.com.parkinson.dan.ittybittymappapp.data.network;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * General module for providing Retrofit, OKHttp client, and Gson deserialiser.
+ * Assumes use of GSON and RXJava
+ *
+ * Created by dan on 28/02/2018.
+ */
+
+@Module
+public class NetModule {
+
+    private String mBaseUrl;
+
+    /**
+     * @param baseUrl base url for this api
+     */
+    public NetModule(String baseUrl) {
+        this.mBaseUrl = baseUrl;
+    }
+
+    @Provides
+    @Singleton
+    Gson provideGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        return gsonBuilder.create();
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient() {
+        return new OkHttpClient();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
+        return new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .baseUrl(mBaseUrl)
+                .client(okHttpClient)
+                .build();
+    }
+
+}
