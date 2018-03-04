5be431c3c468a6881a8d13ea862cd02cd6801a14
Dan <dan@domesticcat.com.au>
2018-03-04 19:25:47 +1100

    updated place model

--- app/src/main/java/au/com/parkinson/dan/ittybittymappapp/domain/place/Place.java
+++ app/src/main/java/au/com/parkinson/dan/ittybittymappapp/domain/place/Place.java
@@ -1,14 +1,48 @@
 package au.com.parkinson.dan.ittybittymappapp.domain.place;
 
 /**
+ * Domain level model for a place.
+ *
  * Created by dan on 3/03/2018.
  */
 
 public class Place {
 
-    String id;
-    Double latitude;
-    Double longitude;
+    private String id;
+    private Double latitude;
+    private Double longitude;
+    private String name;
 
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
 }
