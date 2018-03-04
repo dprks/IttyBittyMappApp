8d77ce36435cbd21280a6cc0468176862162a3d5
Dan <dan@domesticcat.com.au>
2018-03-04 19:25:33 +1100

    Place adapter unit test

---
+++ app/src/test/java/au/com/parkinson/dan/ittybittymappapp/PlaceAdapterTest.java
@@ -0,0 +1,51 @@
+package au.com.parkinson.dan.ittybittymappapp;
+
+/**
+ * Created by dan on 4/03/2018.
+ */
+
+import com.google.gson.Gson;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import au.com.parkinson.dan.ittybittymappapp.data.adapter.GooglePlaceAdapter;
+import au.com.parkinson.dan.ittybittymappapp.data.adapter.PlaceAdapter;
+import au.com.parkinson.dan.ittybittymappapp.data.network.model.place.PlaceSearchResults;
+import au.com.parkinson.dan.ittybittymappapp.data.network.model.place.Result;
+import au.com.parkinson.dan.ittybittymappapp.domain.place.Place;
+import au.com.parkinson.dan.ittybittymappapp.sampleData.SampleJSON;
+
+import static junit.framework.Assert.assertEquals;
+
+public class PlaceAdapterTest {
+
+    Result inputPlace;
+    PlaceAdapter<Result> placeAdapter;
+
+    @Before
+    public void setup() {
+        placeAdapter = new GooglePlaceAdapter();
+
+        Gson gson = new Gson();
+        inputPlace = gson.fromJson(SampleJSON.getSinglePlaceJSON(), Result.class);
+    }
+
+    @Test
+    public void testConversion() {
+        Place outputPlace = placeAdapter.convert(inputPlace);
+        assertEquals(outputPlace.getId(), "ChIJ90260rVG1moRkM2MIXVWBAQ");
+        assertEquals(outputPlace.getName(), "Melbourne");
+        assertEquals(outputPlace.getLatitude(), -37.8136276);
+        assertEquals(outputPlace.getLongitude(), 144.9630576);
+    }
+
+    @Test
+    public void testConversionWithNull() {
+
+    }
+
+
+}
