cd1a7b9d96f7f8b9e10d51ad043f200148f309e9
Dan <dan@domesticcat.com.au>
2018-03-04 16:19:16 +1100

    Added domain layer place model

---
+++ app/src/main/java/au/com/parkinson/dan/ittybittymappapp/domain/place/Place.java
@@ -0,0 +1,14 @@
+package au.com.parkinson.dan.ittybittymappapp.domain.place;
+
+/**
+ * Created by dan on 3/03/2018.
+ */
+
+public class Place {
+
+    String id;
+    Double latitude;
+    Double longitude;
+
+
+}
